87f1d3e919d3691566ee5a715110cd1ad00c4d87
Ariel <ariel96cs@gmail.com>
2019-10-31 12:25:05 -0400

    Added some validations form

--- FlaskApp/search_controller.py
+++ FlaskApp/search_controller.py
@@ -9,15 +9,11 @@ from fuzzy_model.lexer_query import Lexer
 def get_results(query,folder_path):
     r = Reader()
     r.readDirectory(folder_path)
-    print(f"Analicemos la siguiente carpeta {folder_path}")
     l = Lexer()
     p = Parser(l)
-    print(f'Los documentos cargados {r.documents}')
     query = p.parse(query)
-    print(query)
     initial_time = time.time()
     most_similar = rank(query, r.documents)
-    print(most_similar)
     final_time = time.time() - initial_time
     result_info = GeneralResultInfo(len(most_similar), final_time)
 
