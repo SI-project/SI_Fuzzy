9fe6bead5f821fc30605c44ed68b73ccef2523f3
Ariel <ariel96cs@gmail.com>
2019-10-31 11:56:57 -0400

    Now the folder path is entered by hand

--- FlaskApp/forms.py
+++ FlaskApp/forms.py
@@ -1,4 +1,19 @@
 from wtforms import Form, TextField
+from wtforms.fields.core import StringField
+from wtforms import validators
+from os.path import isdir
+
+def validate_folder(form,field):
+    if not isdir(field.data):
+        raise validators.ValidationError("La carpeta no existe!")
 
 class SearchQueryData(Form):
-    query = TextField('Query String')
+    query = TextField('Query String',
+                      [
+                          validators.InputRequired('Se requiere una query!'),
+                          # validators.Regexp(r'')
+                      ])
+    folder_path = StringField('Folder path',[
+        validators.InputRequired('Se requiere de una carpeta!'),
+        validate_folder
+    ])
